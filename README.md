68b27d28beef9b8e396a020c601b38af59a314b3
Pagliaro Alexandre <pagliaro.alexandre@hotmail.com>
2016-01-15 20:07:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,158 @@
+# JQuery file drop
+Easy to use and unopinionated file drag and drop jquery plugin with graceful degradation
+
+## Requirements
+
+- JQuery 1.8.x or higher
+
+## Note
+
+It is recommended to use the unobtrusive driver for this plugin: [behavior-file-drop-container-ujs](https://github.com/xire28/behavior-file-drop-container-ujs)
+
+## Install
+
+### Using Bower
+
+```
+bower install jquery-file-drop --save
+```
+
+### Using Bundler
+
+```
+source 'https://rails-assets.org' do
+	gem 'rails-assets-jquery-file-drop'
+end
+```
+
+## Setup with server side JS responses
+### Define the form
+#### HTML
+Just wrap the file input in a container
+
+`file: form.html`
+```
+<form action="events" method="post" role="form">
+    <h1>Create event</h1>
+    <div class="form-group">
+        <label for="event_name">Name: </label>
+        <input name="event[name]" type="text" class="form-control" id="event_name">
+    </div>
+    <div class="form-group">
+        <label for="event_date">Date:</label>
+        <input name="event[data]" type="date" class="form-control" id="event_date">
+    </div>
+    <div class="form-group">
+        <label for="event_images">Images: </label>
+        <div class="js-file-drop-container">
+            <div class="row" id="files"></div>
+            <div class="row">
+                <h1 class="info">Drop files here or click to upload</h1>
+            </div>
+            <input name="event[images][]" type="file" id="event_images" multiple="true" accept="image/*" />
+        </div>
+    </div>
+    <input name="authenticity_token" type="hidden" value="J7CBxfHalt49OSHp27hblqK20c9PgwJ108nDHX/8Cts=" />
+    <button type="submit" class="btn btn-default">Submit</button>
+</form>
+```
+
+#### JS
+
+Initialize the file drop container with the upload url (required) and info url (optional)
+
+`file: app.js`
+```
+$('#js-file-drop-container').fileDropContainer({
+	url: '/events/new/upload',
+	info: {
+		url: '/events/new/upload/info'
+	}
+})
+```
+
+#### Upload JS response
+
+- Retrieve the uploaded files like you would with a normal form (params, $_FILES, etc.) with the input name
+- Render a JS response (content type: application/javascript) to add HTML for the files that have been uploaded
+
+#### Info JS response
+
+- Retrieve the files like you would with a normal form (params, $_POST/$_GET, etc.) with the input name
+- Render a JS response (content type: application/javascript) to add HTML for the files that are currently uploading
+
+Available infos for each file: 
+- name
+- size
+- type
+- last_modified
+
+Note: You can use `jquery-remote-js-scope` to access files and upload request in the remote JS response to generate a thumbnail with data URI (base64) for example.
+
+### Options
+
+#### Default options
+
+```
+{ 
+	contentType: 'multipart/form-data; charset=UTF-8',
+    method: 'post',
+    processData: false,
+    tokenName: 'authenticity_token',
+    info: {
+        method: 'post',
+        scope: {},
+        scopeOnly: false
+    }
+}
+```
+
+Options are passed to jQuery ajax method, so it is possible to use options listed in the [documentation](http://api.jquery.com/jquery.ajax/).
+
+- Base level: Ajax options for the upload request
+- Nested in the `info` key: Ajax options for the info request
+
+Documentation for the scope options is available [here](https://github.com/xire28/jquery-remote-js-scope).
+
+## Setup without server side JS responses
+
+The following events are triggered on the drop container to allow a client side view implementation
+
+### Events
+
+- `files:upload` parameters: `files` and `uploadRequest`
+- `files:deny` parameter : `deniedFiles`
+
+### Request 
+#### Upload events
+
+- `ajax:beforeSend`
+- `ajax:success`
+- `ajax:complete`
+- `ajax:error`
+
+#### Info events
+
+- `info:ajax:beforeSend`
+- `info:ajax:success`
+- `info:ajax:complete`
+- `info:ajax:error`
+
+Events signatures documentated here: [jQuery-ujs documentation](https://github.com/rails/jquery-ujs/wiki/ajax)
+
+
+##TODO
+
+- [ ] Make a "Wow !" demo
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ jquery-file-drop.js
@@ -0,0 +1,209 @@
+/**
+/* ===================================================
+ *  jquery-file-drop.js v1.0.0
+ *  https://github.com/xire28/jquery-file-drop
+ * ===================================================
+ * 
+ * Easy to use and unopinionated file drag and drop jquery plugin with graceful degradation
+ *
+ * Requires
+ *   - JQuery 1.8.0 or later (https://github.com/jquery/jquery)
+ *   - Modernizr 2.x (https://github.com/modernizr/modernizr)
+ *        Filereader detect
+ *
+ * Recommended
+ *   - JQuery-remote-js-scope latest (https://github.com/xire28/jquery-remote-js-scope)
+ *        Access request scope containing upload request and files from the remote js response
+ *
+ * Released under the MIT license
+ *
+ */
+(function($, window) {
+    $.fileDropContainer = function(el, options) {
+        var base = this,
+            stopEventBehavior = function(e) {
+                e.preventDefault()
+                e.stopPropagation()
+            }
+
+        base.generateOptions = function(userBaseOptions) {
+            // Retrieve nodes and function
+            var baseOptions = $.extend(true, {}, $.fileDropContainer.defaultBaseOptions, userBaseOptions),
+                $el = base.$el,
+                $parentForm = $el.closest('form'),
+                parentForm = $parentForm[0],
+                $tokenInput = $parentForm.find('input[name="' + baseOptions.tokenName + '"]'),
+                trigger = $el.trigger
+
+            // Throw on critical exceptions
+            if (!baseOptions.url) throw 'Cannot create file drop container, upload url is not defined'
+            if (!parentForm) throw 'Cannot create file drop container, parent `form` not found for:\n' + base.el.outerHTML
+
+            // Default generated options that can be overriden
+            var defautGeneratedOptions = {
+                beforeSend: trigger.bind($el, 'ajax:beforeSend'),
+                complete: trigger.bind($el, 'ajax:complete'),
+                success: trigger.bind($el, 'ajax:success'),
+                error: trigger.bind($el, 'ajax:error'),
+                data: {},
+                info: {
+                    data: {}
+                }
+            }
+
+            // Add authentification token if present
+            if ($tokenInput[0]) defautGeneratedOptions.data[baseOptions.tokenName] = $tokenInput.val()
+            if (baseOptions.info.url) {
+                if ($tokenInput[0]) defautGeneratedOptions.info.data[baseOptions.tokenName] = $tokenInput.val()
+
+                // Add triggers for info request
+                $.extend(defautGeneratedOptions.info, {
+                    beforeSend: trigger.bind($el, 'info:ajax:beforeSend'),
+                    success: trigger.bind($el, 'info:ajax:success'),
+                    complete: trigger.bind($el, 'info:ajax:complete'),
+                    error: trigger.bind($el, 'info:ajax:error')
+                })
+            }
+
+            return $.extend(true, {}, defautGeneratedOptions, baseOptions)
+        }
+
+        base.acceptFiles = function(files, acceptPattern) {
+            // Filter using files using accept attribute format
+            var $el = base.$el,
+                deniedFiles = [],
+                acceptedFiles = $.grep(files, function(file) {
+                    var accept = new window.RegExp((acceptPattern || '*').replace('*', '.*').replace('+', '\+').replace('.', '\.')).test(file.type)
+                    if (!accept) deniedFiles.push(file)
+                    return accept
+                })
+
+            // Trigger DOM event to allow implementation for denied files
+            $el.trigger('files:deny', [deniedFiles])
+
+            return acceptedFiles
+        }
+
+        base.processFiles = function(files, accept) {
+            // Transform FileList to an Array to be able to apply filter using jQuery
+            return base.acceptFiles($.makeArray(files), accept)
+        }
+
+        base.sendFileInfos = function(name, files, options) {
+            if (!options.scopeOnly) {
+                // JQuery serialization don't work on File instances, so transform them to plain objects
+                var dataEntry = {},
+                    fileInfos = $.map(files, function(file) {
+                        return {
+                            name: file.name,
+                            size: file.size,
+                            type: file.type,
+                            last_modified: file.lastModified
+                        }
+                    })
+
+                dataEntry[name] = fileInfos
+
+                $.extend(options, {
+                    data: dataEntry
+                })
+            }
+
+            $.ajax(options)
+        }
+
+        base.sendFiles = function(name, files, options) {
+            var formData = new window.FormData()
+
+            $.each(files, function(_, file) {
+                formData.append(name, file)
+            })
+
+            // Copy authentification token from options into the formData instance
+            if (options.data[options.tokenName]) formData.append(options.tokenName, options.data[options.tokenName])
+
+            return $.ajax($.extend({}, options, {
+                data: formData
+            }))
+        }
+
+        base.listener = function($fileInput, e) {
+            stopEventBehavior(e)
+
+            var name = $fileInput.attr('name'),
+                // Remove array notation from input name, is be automatically added by jQuery serialization
+                baseName = name.replace(/\[\]$/, ''),
+                files = base.processFiles(e.originalEvent.dataTransfer ? e.originalEvent.dataTransfer.files : e.target.files, $fileInput.attr('accept')),
+                options = base.options
+
+            if (files.length) {
+                var uploadRequest = base.sendFiles(name, files, options)
+
+                // Trigger DOM event to allow implementation for uploading files (e.g. listen to progress event to update progressbar, generate thumbnail using file data URI, etc.)
+                $el.trigger('files:upload', [files, uploadRequest])
+
+                if (options.info.url) {
+                    // Create local variable for js response to access upload request and files (requires jquery-remote-js-scope plugin to be processed)
+                    $.extend(options.info.scope, {
+                        files: files,
+                        uploadRequest: uploadRequest
+                    })
+                    base.sendFileInfos(baseName, files, options.info)
+                }
+            }
+        }
+
+        base.init = function(options) {
+            if (window.Modernizr.filereader) {
+                var $el = base.$el,
+                    $fileInput = $el.find('input[type="file"]').first(),
+                    listenerWithInput = base.listener.bind(base, $fileInput)
+
+                base.options = base.generateOptions(options)
+
+                $el
+                    .on('drop', listenerWithInput)
+                    .on('dragover dragenter', stopEventBehavior)
+                    .on('click', function() {
+                        $fileInput.click()
+                    })
+
+                $fileInput
+                    .on('click', function(e) {
+                        e.stopPropagation()
+                    })
+                    .on('change', listenerWithInput)
+                    .detach()
+            }
+        }
+
+        base.$el = $(el)
+        base.el = el
+        base.$el.data('fileDropContainer', base)
+        base.init(options)
+    }
+
+    // Default options that can be overriden
+    $.fileDropContainer.defaultBaseOptions = {
+        contentType: 'multipart/form-data; charset=UTF-8',
+        method: 'post',
+        processData: false,
+        tokenName: 'authenticity_token',
+        info: {
+            method: 'post',
+            scope: {},
+            scopeOnly: false
+        }
+    }
+
+    $.fn.fileDropContainer = function(options) {
+        return this.each(function() {
+            (new $.fileDropContainer(this, options))
+        })
+    }
+
+    $.fn.getfileDropContainer = function() {
+        this.data('fileDropContainer')
+    }
+
+})(jQuery, window)
